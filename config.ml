2bc5d56d1fc37e99bdd4244f3daca5546b341ae3
Stephane Glondu <steph@glondu.net>
2014-10-20 15:37:29 +0200

    Use maude and full-maude.maude from $PATH

--- config.ml
+++ config.ml
@@ -17,34 +17,38 @@
 (* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.              *)
 (****************************************************************************)
 
+let find_in_path x =
+  let path = Str.split (Str.regexp ":") (Sys.getenv "PATH") in
+  List.find
+    Sys.file_exists
+    (List.map (fun d -> Filename.concat d x) path)
+
+let maude_binary =
+  try
+    find_in_path "maude"
+  with
+    | Not_found ->
+        Printf.eprintf "Cound not find maude in PATH!\n" ;
+        Printf.eprintf "It is likely that tamarin-prover won't work.\n" ;
+        exit 1
+
+let full_maude =
+  try
+    find_in_path "full-maude.maude"
+  with
+    | Not_found ->
+       Printf.eprintf "Could not find full-maude.maude in PATH!\n";
+       exit 1
+
 let maude_command =
-  let home = Sys.getenv "HOME" in
-  let maude_dir = Filename.concat home "/soft/maude" in
-  let maude = Filename.concat maude_dir "maude" in
-  let full_maude = Filename.concat maude_dir "full-maude.maude" in
-    maude ^ " -batch -no-banner -no-ansi-color " ^ full_maude
+  maude_binary ^ " -batch -no-banner -no-ansi-color " ^ full_maude
 
 let tamarin_binary =
   try
-    let path = Str.split (Str.regexp ":") (Sys.getenv "PATH") in
-      List.find
-        Sys.file_exists
-        (List.map (fun d -> Filename.concat d "tamarin-prover") path)
+    find_in_path "tamarin-prover"
   with
     | Not_found ->
         Printf.eprintf "Cound not find tamarin-prover in PATH!\n" ;
         Printf.eprintf "You may need to add something like \
                         ~/.cabal/bin to it.\n" ;
-        exit 1
-
-let maude_binary =
-  try
-    let path = Str.split (Str.regexp ":") (Sys.getenv "PATH") in
-      List.find
-        Sys.file_exists
-        (List.map (fun d -> Filename.concat d "maude") path)
-  with
-    | Not_found ->
-        Printf.eprintf "Cound not find maude in PATH!\n" ;
-        Printf.eprintf "It is likely that tamarin-prover won't work.\n" ;
         exit 1
