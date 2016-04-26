7d75b1f168366d2fdd2e915d532a42fa1a156d32
David Baelde <david.baelde@ens-lyon.org>
2016-04-26 13:27:34 +0200

    Fix bug/typo in pretty printing of plus

--- src/term.ml
+++ src/term.ml
@@ -117,7 +117,7 @@ let rec show_term = function
       show_term (Fun("test", term_list))
   | Fun("world", [w; ws]) -> show_term w ^ "." ^ show_term ws
   | Fun("zero",[]) -> "0"
-  | Fun("plus",[t1;t2]) -> (show_term t1)^"+"^(show_term t1)
+  | Fun("plus",[t1;t2]) -> (show_term t1)^"+"^(show_term t2)
   | Fun(f, l) ->
       (f ^
 	 (if l <> [] then "(" else "") ^
